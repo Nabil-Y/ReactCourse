e8ab9debaf792de773882844a9bbc6fef06f4fdb
nabil-y <nabil.yassine.dev@gmail.com>
2022-03-30 16:56:01 +0200

    Start new project

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+import "./ExpenseItem.css";
+
+export default function ExpenseItem(props) {
+  return (
+    <div className="expense-item" key={props.id}>
+      <div>{props.date.toLocaleDateString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}€</div>
+      </div>
+    </div>
+  );
+}
