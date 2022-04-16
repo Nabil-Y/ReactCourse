4d852061b166f157ca81816a991c60e4f2dd0cd1
nabil-y <nabil.yassine.dev@gmail.com>
2022-04-16 02:31:49 +0200

    New project setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import * as ReactDOMClient from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const container = document.getElementById("root");
+import './index.css';
+import App from './App';
 
-const root = ReactDOMClient.createRoot(container);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
