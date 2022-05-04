e3d62d51c86990dcb017bfffa122551d575bf6d0
nabil-y <nabil.yassine.dev@gmail.com>
2022-05-04 17:25:36 +0200

    start food app

--- src/App.js
+++ src/App.js
@@ -1,46 +1,10 @@
-import React, { useEffect, useState } from 'react';
-
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
-import AuthContext from './components/store/auth-context';
+import Header from './components/Layout/Header';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const storedUserLogInfo = localStorage.getItem('isLoggedIn');
-
-    if (storedUserLogInfo === '1') {
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem('isLoggedIn', '1');
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.setItem('isLoggedIn', '0');
-    setIsLoggedIn(false);
-  };
-
   return (
-    <AuthContext.Provider
-      value={{
-        isLoggedIn: isLoggedIn,
-        onLogout: logoutHandler,
-      }}
-    >
-      <MainHeader onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home />}
-      </main>
-    </AuthContext.Provider>
+    <>
+      <Header />
+    </>
   );
 }
 
