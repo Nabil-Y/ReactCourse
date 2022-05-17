438e949ae989a992bdf864b169bc6dd12b09e3cf
nabil-y <nabil.yassine.dev@gmail.com>
2022-05-17 18:13:03 +0200

    Complete project

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import classes from './Header.module.css';
-import img from '../../assets/meals.jpg';
-import HeaderCartButton from './HeaderCartButton';
+import React from "react";
+import classes from "./Header.module.css";
+import img from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
-      <div className={classes['main-image']}>
+      <div className={classes["main-image"]}>
         <img src={img} alt="Meals" />
       </div>
     </>
