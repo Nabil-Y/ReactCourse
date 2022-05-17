9a785d7ffcd68e99cfd49faaa28d2aa7f0b2e258
nabil-y <nabil.yassine.dev@gmail.com>
2022-05-17 21:19:56 +0200

    setup project

--- src/components/UI/Card.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import classes from './Card.module.css';
-
-const Card = ({ children, className }) => {
-  const styles = `${className} ${classes.card}`;
-  return <div className={styles}>{children} </div>;
-};
-
-export default Card;
