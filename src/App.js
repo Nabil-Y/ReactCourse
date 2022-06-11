7882c2aa4f75bf24932e6ddf6d6d7d7e44cfd00a
nabil-y <nabil.yassine.dev@gmail.com>
2022-06-12 00:45:59 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
-
-import './App.css';
+import { useState } from "react";
+import Button from "./components/UI/Button/Button";
+import "./App.css";
+import DemoOutput from "./components/UI/Button/Demo/DemoOutput";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  const clickHandler = () => setShowParagraph((prevState) => !prevState);
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <DemoOutput show={showParagraph} />}
+      <Button onClick={clickHandler}>Toggle Paragraph</Button>
     </div>
   );
 }
