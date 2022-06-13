ab09898246fa8592e41ba90676da4592f45f0169
nabil-y <nabil.yassine.dev@gmail.com>
2022-06-13 11:07:43 +0200

    start module 14

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import { useState } from "react";
-import Button from "./components/UI/Button/Button";
-import "./App.css";
-import DemoOutput from "./components/Demo/DemoOutput";
+import React from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState(false);
-  const clickHandler = () => setShowParagraph((prevState) => !prevState);
+  const dummyMovies = [
+    {
+      id: 1,
+      title: 'Some Dummy Movie',
+      openingText: 'This is the opening text of the movie',
+      releaseDate: '2021-05-18',
+    },
+    {
+      id: 2,
+      title: 'Some Dummy Movie 2',
+      openingText: 'This is the second opening text of the movie',
+      releaseDate: '2021-05-19',
+    },
+  ];
 
   return (
-    <div className="app">
-      <h1>Hi there!</h1>
-      {showParagraph && <DemoOutput show={showParagraph} />}
-      <Button onClick={clickHandler}>Toggle Paragraph</Button>
-    </div>
+    <React.Fragment>
+      <section>
+        <button>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={dummyMovies} />
+      </section>
+    </React.Fragment>
   );
 }
 
