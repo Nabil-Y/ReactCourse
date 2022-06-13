634eadce686a8d964afae2c8ebf66d7f9ec1ae6d
nabil-y <nabil.yassine.dev@gmail.com>
2022-06-14 00:32:34 +0200

    init section 16

--- src/App.js
+++ src/App.js
@@ -1,48 +1,10 @@
-import React, { useEffect, useState } from "react";
-import useHttp from "./hooks/use-http";
-import Tasks from "./components/Tasks/Tasks";
-import NewTask from "./components/NewTask/NewTask";
+import SimpleInput from './components/SimpleInput';
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-
-  const httpData = useHttp();
-
-  const { isLoading, error, sendRequest: fetchTasks } = httpData;
-
-  useEffect(() => {
-    const transformTasks = (taskObj) => {
-      const loadedTasks = [];
-
-      for (const taskKey in taskObj) {
-        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    };
-
-    fetchTasks(
-      {
-        url: "https://react-http-899a2-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
-      },
-      transformTasks
-    );
-  }, [fetchTasks]);
-
-  const taskAddHandler = (task) => {
-    setTasks((prevTasks) => prevTasks.concat(task));
-  };
-
   return (
-    <React.Fragment>
-      <NewTask onAddTask={taskAddHandler} />
-      <Tasks
-        items={tasks}
-        loading={isLoading}
-        error={error}
-        onFetch={fetchTasks}
-      />
-    </React.Fragment>
+    <div className="app">
+      <SimpleInput />
+    </div>
   );
 }
 
